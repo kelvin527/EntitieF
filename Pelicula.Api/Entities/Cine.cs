5ffbdd703bf16fb503d54ed621df2c1c12045093
kelvin <felixkelvin00@gmail.com>
2022-10-07 16:23:42 -0400

    decimal

---
+++ Pelicula.Api/Entities/Cine.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Pelicula.Api.Entities
+{
+    public class Cine
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        [Precision(9,2)]//este metedo sirve para ponerle el espacio en digitos que queramos 
+       //              que tenga nuestra propiedad con tipo de dato decimal
+        public decimal Precio { get; set; }
+    }
+}
--- Pelicula.Api/Entities/Context.cs
+++ Pelicula.Api/Entities/Context.cs
@@ -11,11 +11,14 @@ namespace Pelicula.Api.Entities
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
-            //asi se hace una llave primaria en un api fluente
-           // modelBuilder.Entity<Genero>().HasKey(x => x.Id);
+            /*asi se hace una llave primaria en un api fluente
+           // modelBuilder.Entity<Genero>().HasKey(x => x.Id);*/
+         /* modelBuilder.Entity<Cine>().Property(pro=> pro.Precio).HasPrecision(9,2);
+          dandole preci*/
         }
 
         public DbSet<Genero> Generos { get; set; }
         public DbSet<Actor> Actor { get; set; }
+        public DbSet<Cine> Cines { get; set; }
     }
 }
